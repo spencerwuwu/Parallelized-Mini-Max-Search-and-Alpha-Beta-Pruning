c8569123bd337d181e083d2f2d66718b5702e3a0
Tsung-en Hsiao <joshua5201.eecs03@nctu.edu.tw>
2017-01-04 00:24:48 +0800

    Improve search

--- src/serial/Search.cpp
+++ src/serial/Search.cpp
@@ -7,7 +7,6 @@ ChessBoard* MinMax(ChessBoard* board, int dept_limit){
 	return MaxMove(board, dept_limit, 0);
 }
 
-
 static ChessBoard* _MinMaxMove(ChessBoard* board, int dept_limit, int dept, enum FindAction action);
 ChessBoard* MaxMove(ChessBoard* board, int dept_limit, int dept){
     return _MinMaxMove(board, dept_limit, dept, FIND_MAX);
@@ -39,7 +38,7 @@ static inline enum FindAction another(enum FindAction action)
 static ChessBoard* _MinMaxMove(ChessBoard* board, int dept_limit, int dept, enum FindAction action) {
 	vector<ChessBoard*> moves;
 	ChessBoard* best_move = NULL;
-	ChessBoard* best_real_move = new ChessBoard;
+    int best_real_move = 0;
 	ChessBoard* move = NULL;
 
 	if (dept >= dept_limit) {//if depth limit is reached
@@ -50,10 +49,16 @@ static ChessBoard* _MinMaxMove(ChessBoard* board, int dept_limit, int dept, enum
 			move = _MinMaxMove( moves[i], dept_limit, dept+1, another(action));
 			if (best_move == NULL || cmp_move(action, move, best_move)) {
 				best_move = move;
-				*best_real_move = *moves[i];
+                best_real_move = i;
 			}
 		}
-		return best_real_move;
+
+        ChessBoard* res = new ChessBoard;
+        *res = *moves[best_real_move];
+        for (int i = 0; i < moves.size(); i++) {
+            delete moves[i];
+        }
+		return res;
 	}
 }
 
